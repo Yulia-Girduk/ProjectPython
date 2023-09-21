5bc21ef6b61f4d3418bd8850aceacbe15b7456a2
Nick Gir <mbiko@ya.ru>
2023-09-21 11:13:00 +0300

    add file view

---
+++ view.py
@@ -0,0 +1,39 @@
+import text
+from datetime import datetime
+from operator import itemgetter
+
+
+def menu() -> int:
+    print(text.menu[0])
+    for i in range(1, len(text.menu)):
+        print(f"\t{i}. {text.menu[i]}")
+    while True:
+        selected = input(text.select_menu)
+        if int(selected) > 0 and int(selected) <= len(text.menu):
+            return int(selected)
+        print(text.invalid_selection)
+
+
+def show_nodes(note: dict, message):
+    if note:
+        # sorted(note.items(), key=lambda x: x["title"], reverse=True)
+        for index, note in note.items():
+            print(f'{index:<3}. {note.get("title"):<50}\n{note.get("body"):<250}\n{note.get("date"):<20}')
+    else:
+        print_message(message)
+
+
+def print_message(message):
+    print(message)
+
+
+def add_note():
+    new_note = {}
+    for key, value in text.new_note.items():
+        new_note[key] = input(value)
+    new_note['date'] = datetime.now().isoformat(sep=',', timespec='minutes')
+    return new_note
+
+
+def view_input(text: str):
+    return input(text)
